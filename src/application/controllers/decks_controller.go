a1bf6a5eabdf91829d51bdd972f4411919e3dda8
Fernando Oliveira <nandooliveira.al@gmail.com>
2022-07-05 23:48:17 -0300

    Allow to pass cards to be in the deck

--- src/application/controllers/decks_controller.go
+++ src/application/controllers/decks_controller.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
@@ -11,13 +12,50 @@ import (
 	"github.com/nandooliveira/deck-api/src/application/models"
 )
 
+var (
+	translateSuits = map[string]models.Suit{
+		"C": models.CLUBS,
+		"D": models.DIAMONDS,
+		"H": models.HEARTS,
+		"S": models.SPADES,
+	}
+
+	translateRanks = map[string]models.Rank{
+		"A":  models.ACE,
+		"2":  models.TWO,
+		"3":  models.THREE,
+		"4":  models.FOUR,
+		"5":  models.FIVE,
+		"6":  models.SIX,
+		"7":  models.SEVEN,
+		"8":  models.EIGHT,
+		"9":  models.NINE,
+		"10": models.TEN,
+		"J":  models.JACK,
+		"Q":  models.QUEEN,
+		"K":  models.KING,
+	}
+)
+
 type DecksController struct{}
 
 func (this *DecksController) Create(w http.ResponseWriter, r *http.Request) {
 	shuffled := r.URL.Query().Get("shuffled")
-	deck, err := models.NewDeck(func(o *models.Options) {
+
+	codeCards := r.URL.Query().Get("cards")
+
+	options := func(o *models.Options) {
 		o.Shuffled, _ = strconv.ParseBool(shuffled)
-	})
+		if codeCards != "" {
+			splittedCards := strings.Split(codeCards, ",")
+
+			for _, cardCode := range splittedCards {
+				o.Cards = append(o.Cards, models.NewCard(translateRanks[cardCode[0:1]], translateSuits[cardCode[1:2]]))
+			}
+		}
+	}
+
+	deck, err := models.NewDeck(options)
 
 	if err != nil {
 		handleParamError(w, err.Error())
